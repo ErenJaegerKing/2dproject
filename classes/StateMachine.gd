971226b9c1a3eab59fdcbcdcf3a3f53a4c32855f
Jaeger Eren <erenjaegerking@qq.com>
2025-04-20 17:33:19 +0800

    feat:有限状态机

---
+++ classes/StateMachine.gd
@@ -0,0 +1,24 @@
+## 角色状态机
+class_name StateMachine
+extends Node
+
+# 状态变量与setter
+var current_state: int = -1:
+	set(v):
+		owner.transition_state(current_state,v)
+		current_state = v
+
+func _ready() -> void:
+	# 等待父节点(owner)完成初始化
+	await owner.ready
+	current_state = 0
+
+# 状态更新循环 (_process)
+func _physics_process(delta: float) -> void:
+	while true:
+		var next := owner.get_next_state(current_state) as int
+		if current_state == next:
+			break
+		current_state = next
+		
+	owner.tick_physics(current_state, delta)
--- player.gd
+++ player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
+enum State {
+	IDLE,
+	RUNNING,
+	JUMP,
+	FALL,
+	LANDING
+}
+
+const GROUND_STATUS := [State.IDLE,State.RUNNING,State.LANDING]
 const RUN_SPEED := 160.0
 const FLOOR_ACCELERATION := RUN_SPEED / 0.2
 const AIR_ACCELERATION := RUN_SPEED / 0.02
-const JUMP_VELOCITY := -320.0
+const JUMP_VELOCITY := -400.0
 
-var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var default_gravity := ProjectSettings.get("physics/2d/default_gravity") as float
+var is_first_tick := false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite_2d: Sprite2D = $Sprite2D
@@ -17,46 +27,101 @@ var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):
 		jump_request_timer.start()
-	# 可变高度跳跃
-	if event.is_action_released("jump") and velocity.y < JUMP_VELOCITY / 2:
-		velocity.y = JUMP_VELOCITY / 2
+	# 可变高度跳跃	
+	if event.is_action_released("jump"):
+		jump_request_timer.stop()
+		if velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
 	
 
-func _physics_process(delta: float) -> void:
+func tick_physics(state: State,delta: float) -> void:
+	match state:
+		State.IDLE:
+			move(default_gravity,delta)
+		State.RUNNING:
+			move(default_gravity,delta)
+		State.JUMP:
+			move(0.0 if is_first_tick else default_gravity, delta)
+		State.FALL:
+			move(default_gravity,delta)
+		State.LANDING:
+			stand(delta)
+	is_first_tick = false
+
+func move(gravity: float, delta: float) -> void:
 	var direction := Input.get_axis("move_left","move_right")
 	# 走到跑-均匀加速 
 	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
 	velocity.x = move_toward(velocity.x, direction * RUN_SPEED,acceleration  * delta) 
 	velocity.y += gravity * delta
 	
-	# 跳
-	var can_jump := is_on_floor() or coyote_timer.time_left > 0
-	var should_jump := can_jump and jump_request_timer.time_left > 0
-	if should_jump:
-		velocity.y = JUMP_VELOCITY
-		coyote_timer.stop()
-		jump_request_timer.stop()
-	
-	# 角色动画切换
-	if is_on_floor():
-		if is_zero_approx(direction) and is_zero_approx(velocity.x):
-			animation_player.play("idle")
-		else:
-			animation_player.play("running")
-	else:
-		animation_player.play("jump")
-	
 	# 角色反转
 	if not is_zero_approx(direction):
 		sprite_2d.flip_h = direction < 0
 	
-	var was_on_floor :=  is_on_floor()
+	move_and_slide()
+
+func stand(delta: float) -> void:
+	var acceleration := FLOOR_ACCELERATION if is_on_floor() else AIR_ACCELERATION
+	velocity.x = move_toward(velocity.x, 0.0,acceleration  * delta) 
+	velocity.y += default_gravity * delta
 	move_and_slide()
 	
+func get_next_state(state: State) -> State:
 	# 郊狼时间
-	if is_on_floor() != was_on_floor:
-		if was_on_floor and not should_jump:
-			coyote_timer.start()
-		else:
+	var can_jump := is_on_floor() or coyote_timer.time_left > 0
+	# 预输入
+	var should_jump := can_jump and jump_request_timer.time_left > 0
+	if should_jump:
+		return State.JUMP
+
+	# 获得水平输入方向
+	var direction := Input.get_axis("move_left","move_right")
+	# 判断角色是否静止
+	var is_still := is_zero_approx(direction) and is_zero_approx(velocity.x)
+	
+	# 根据当前状态决定下一个状态
+	match state:
+		State.IDLE:
+			if not is_on_floor():
+				return State.FALL
+			if not is_still:
+				return State.RUNNING
+		State.RUNNING:
+			if not is_on_floor():
+				return State.FALL
+			if is_still:
+				return State.IDLE
+		State.JUMP:
+			if velocity.y >= 0:
+				return State.FALL
+		State.FALL:
+			if is_on_floor():
+				return State.LANDING if is_still else State.RUNNING
+		State.LANDING:
+			if not animation_player.is_playing():
+				return State.IDLE
+	return state
+	
+func transition_state(from: State,to: State) -> void:
+	if from not in GROUND_STATUS and to in GROUND_STATUS:
+		coyote_timer.stop()
+	
+	match to:
+		State.IDLE:
+			animation_player.play("idle")
+		State.RUNNING:
+			animation_player.play("running")
+		State.JUMP:
+			animation_player.play("jump")
+			velocity.y = JUMP_VELOCITY
 			coyote_timer.stop()
-		
+			jump_request_timer.stop()
+		State.FALL:
+			animation_player.play("fall")
+			if from in GROUND_STATUS:
+				coyote_timer.start()
+		State.LANDING:
+			animation_player.play("landing")
+			
+	is_first_tick = true
